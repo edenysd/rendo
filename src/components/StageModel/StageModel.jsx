484ca9fd7d761b5038f76a70800bc57c53aa9154
Edenys Deniz González <holofist1@gmail.com>
2024-01-28 21:37:03 -0500

    fix: remove stats from render

--- src/components/StageModel/StageModel.jsx
+++ src/components/StageModel/StageModel.jsx
@@ -7,6 +7,7 @@ function StageModel({ selectedWall }) {
     <div className="relative w-full h-full" id="canvas-container">
       <Canvas camera={{ position: [5, 5, -5] }}>
         <Scene selectedWall={selectedWall} />
+
         <ambientLight intensity={6} />
         <directionalLight
           color="white"
@@ -14,10 +15,8 @@ function StageModel({ selectedWall }) {
           distance={20}
           intensity={2}
         />
-        {/* <directionalLight color="white" position={[5, 5, 5]} intensity={0.4} /> */}
 
         <OrbitControls target={[0, 0, 2]} />
-        <Stats />
       </Canvas>
     </div>
   );
