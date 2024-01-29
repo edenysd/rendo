55325388b2d52535802d114f6ea42a78326fadbe
Edenys Deniz González <holofist1@gmail.com>
2024-01-28 20:02:01 -0500

    feat: add clear selection button

--- src/components/WallPicker/WallPicker.jsx
+++ src/components/WallPicker/WallPicker.jsx
@@ -11,49 +11,59 @@ const WallPicker = ({
   handleChangeSelectedWall,
   wallsList,
   ...props
-}) => (
-  <div className={props.className}>
-    <Select.Root
-      onValueChange={(newSelectedWall) => {
-        handleChangeSelectedWall(newSelectedWall);
-      }}
-      value={selectedWall?.name}
-    >
-      <Select.Trigger className="w-[200px] justify-between inline-flex items-center rounded px-[15px] text-[13px] leading-none h-[35px] gap-[5px] bg-white text-violet11 shadow-[0_2px_10px] shadow-black/10 hover:bg-mauve3 focus:shadow-[0_0_0_2px] focus:shadow-black data-[placeholder]:text-violet9 outline-none">
-        <Select.Value placeholder="Select a wall…" />
-        <Select.Icon className="text-violet11">
-          <ChevronDownIcon />
-        </Select.Icon>
-      </Select.Trigger>
-      <Select.Portal>
-        <Select.Content className="overflow-hidden bg-white rounded-md shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] z-20">
-          <Select.ScrollUpButton className="flex items-center justify-between h-[25px] bg-white text-violet11 cursor-default">
-            <ChevronUpIcon />
-          </Select.ScrollUpButton>
-          <Select.Viewport className="p-[5px]">
-            <Select.Group>
-              {wallsList.map((wall) => (
-                <SelectItem value={wall.name} key={wall.name}>
-                  {wall.mesh}
-                </SelectItem>
-              ))}
-            </Select.Group>
-          </Select.Viewport>
-          <Select.ScrollDownButton className="flex items-center justify-center h-[25px] bg-white text-violet11 cursor-default">
+}) => {
+  return (
+    <div className={props.className}>
+      <Select.Root
+        onValueChange={(newSelectedWall) => {
+          handleChangeSelectedWall(newSelectedWall);
+        }}
+        value={selectedWall?.name || ""}
+      >
+        <Select.Trigger className="w-[200px] justify-between inline-flex items-center rounded px-[15px] text-[15px] leading-none h-[35px] gap-[5px] bg-white text-violet11 shadow-[0_2px_10px] shadow-black/10 hover:bg-mauve3 focus:shadow-[0_0_0_2px] focus:shadow-black data-[placeholder]:text-violet9 outline-none mr-3">
+          <Select.Value placeholder="Select a wall…" />
+          <Select.Icon className="text-violet11">
             <ChevronDownIcon />
-          </Select.ScrollDownButton>
-        </Select.Content>
-      </Select.Portal>
-    </Select.Root>
-  </div>
-);
+          </Select.Icon>
+        </Select.Trigger>
+        <Select.Portal>
+          <Select.Content className="overflow-hidden bg-white rounded-md shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] z-20">
+            <Select.ScrollUpButton className="flex items-center justify-between h-[25px] bg-white text-violet11 cursor-default">
+              <ChevronUpIcon />
+            </Select.ScrollUpButton>
+            <Select.Viewport className="p-[5px]">
+              <Select.Group>
+                {wallsList.map((wall) => (
+                  <SelectItem value={wall.name} key={wall.name}>
+                    {wall.label}
+                  </SelectItem>
+                ))}
+              </Select.Group>
+            </Select.Viewport>
+            <Select.ScrollDownButton className="flex items-center justify-center h-[25px] bg-white text-violet11 cursor-default">
+              <ChevronDownIcon />
+            </Select.ScrollDownButton>
+          </Select.Content>
+        </Select.Portal>
+      </Select.Root>
+      <button
+        className="inline-flex items-center justify-center rounded font-medium text-[15px] px-[15px] leading-[35px] h-[35px] bg-white text-red-500 outline-1 hover:bg-mauve3 focus:shadow-[0_0_0_2px] focus:shadow-black"
+        onClick={() => {
+          handleChangeSelectedWall(null);
+        }}
+      >
+        Clear Selection
+      </button>
+    </div>
+  );
+};
 
 const SelectItem = React.forwardRef(
   ({ children, className, ...props }, forwardedRef) => {
     return (
       <Select.Item
         className={
-          "cursor-pointer text-[13px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1"
+          "cursor-pointer text-[15px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1"
         }
         {...props}
         ref={forwardedRef}
